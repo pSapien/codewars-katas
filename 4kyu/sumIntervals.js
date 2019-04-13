51cf04ef2d1c7dd2f0611dd51ddf47cafa62f106
Prateek Thapa <prateekthapa26@gmail.com>
2019-04-13 18:33:56 +0545

    added sum interval problem

---
+++ 4kyu/sumIntervals.js
@@ -0,0 +1,14 @@
+/*** ===================================================================== ***\
+ ** - PROBLEM ------------------------------------------------------ **
+ * ========================================================================= *
+ *
+ 
+ Write a function called sumIntervals/sum_intervals() that accepts an array of intervals, and returns the sum of all the interval lengths. Overlapping intervals should only be counted once. 
+ Intervals are represented by a pair of integers in the form of an array.
+  The first value of the interval will always be less than the second value. 
+  Interval example: [1, 5] is an interval from 1 to 5. The length of this interval is 4.
+ 
+
+ * https://www.codewars.com/kata/sum-of-intervals/train/javascript
+ 
+\*** ===================================================================== ***/

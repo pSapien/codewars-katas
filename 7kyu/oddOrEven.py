4c498c24664dc3ff6b2d495dceb0a009c7cd457b
Prateek Thapa <prateekthapa26@gmail.com>
2019-06-02 20:02:33 +0545

    oddOrEven [ py ]

---
+++ 7kyu/oddOrEven.py
@@ -0,0 +1,20 @@
+'''
+  Given an array of numbers, determine whether the sum of all of the numbers is odd or even.
+
+  Give your answer in string format as 'odd' or 'even'.
+
+  If the input array is empty consider it as: [0] (array with a zero).
+
+  Example:
+
+  oddOrEven([0]) returns "even"
+  oddOrEven([2, 5, 34, 6]) returns "odd"
+  oddOrEven([0, -1, -5]) returns "even"
+'''
+
+
+def oddOrEven(arr):
+    if sum(arr) % 2 == 0:
+        return 'even'
+
+    return 'odd'
